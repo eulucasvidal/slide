8ae4def1861c14391cdaa44bff82b1a7562cba42
Lucas Vidal <luccas.lima12@gmail.com>
2023-09-08 17:07:58 -0300

    organizando os slides

--- js/slide.js
+++ js/slide.js
@@ -2,6 +2,11 @@ export default class Slide{
     constructor(slide, wrapper){
         this.slide = document.querySelector(slide);
         this.wrapper = document.querySelector(wrapper);
+        this.dist ={
+            finalPosition:0,
+            startX:0,
+            movement:0
+        }
     }
 
     onStart(event){
